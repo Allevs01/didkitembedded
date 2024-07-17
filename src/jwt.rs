a35c0f074879a37b1e43b195cd1ffa845ab0e36c
allevs01 <stefanoallevi01@gmail.com>
2024-07-17 23:02:32 +0200

    rebuild the program without didkit

--- src/jwt.rs
+++ src/jwt.rs
@@ -2,9 +2,13 @@
 // cargo run --example issue ldp > examples/vc.jsonld
 // cargo run --example issue jwt > examples/vc.jwt
 
-use didkit::{ DIDMethod, Document};
-use example::DIDExample;
 use tokio;
+use async_trait::async_trait;
+use ssi_dids::Document;
+use ssi_dids::did_resolve::{
+    DIDResolver, DocumentMetadata, ResolutionInputMetadata, ResolutionMetadata,
+    ERROR_NOT_FOUND
+};
 
 
 
@@ -13,7 +17,7 @@ async fn main()-> Result<(), Box<dyn std::error::Error>>{
     let key_str = include_str!(r"../chiave_str.json");
     //funzione per creare vc prendo key, resolver, did_issuer, verification method
     let key: ssi::jwk::JWK = serde_json::from_str(key_str).unwrap();
-    let resolver = &DIDExample;
+    let resolver = &DIDExampleStatic;
     let vc = serde_json::json!({
         "@context": [
             "https://www.w3.org/2018/credentials/v1",
@@ -59,64 +63,43 @@ async fn main()-> Result<(), Box<dyn std::error::Error>>{
 }
 
 
-pub mod example {
-    use didkit::ssi::did_resolve::{
-        DIDResolver, DocumentMetadata, ResolutionInputMetadata, ResolutionMetadata,
-        ERROR_NOT_FOUND, TYPE_DID_LD_JSON,
-    };
-    use crate::{DIDMethod, Document};
-    use async_trait::async_trait;
 
-    const DOC_JSON_TEST_MINE: &str = include_str!(r"../did-example-mine.json");
+pub struct DIDExampleStatic;
 
-    /// An implementation of `did:example`.
-    ///
-    /// For use with [VC Test Suite](https://github.com/w3c/vc-test-suite/) and in other places.
-    pub struct DIDExample;
-
-    #[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
-    #[cfg_attr(not(target_arch = "wasm32"), async_trait)]
-    impl DIDMethod for DIDExample {
-        fn name(&self) -> &'static str {
-            "example"
-        }
-
-        fn to_resolver(&self) -> &dyn DIDResolver {
-            self
-        }
-    }
-
-    #[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
-    #[cfg_attr(not(target_arch = "wasm32"), async_trait)]
-    impl DIDResolver for DIDExample {
-        async fn resolve(
-            &self,
-            did: &str,
-            _input_metadata: &ResolutionInputMetadata,
-        ) -> (
-            ResolutionMetadata,
-            Option<Document>,
-            Option<DocumentMetadata>,
-        ) {
-            let doc_str = match did {
-                "did:key:z6MkjF6Srb2uTSHVtjA53e59pUWJEY2QZzMkh9w198mhZmzB" => DOC_JSON_TEST_MINE,
-                _ => return (ResolutionMetadata::from_error(ERROR_NOT_FOUND), None, None),
-            };
-            let doc: Document = match serde_json::from_str(doc_str) {
-                Ok(doc) => doc,
-                Err(err) => {
-                    return (ResolutionMetadata::from_error(&err.to_string()), None, None);
-                }
-            };
-            (
-                ResolutionMetadata {
-                    error: None,
-                    content_type: Some(TYPE_DID_LD_JSON.to_string()),
-                    property_set: None,
-                },
-                Some(doc),
-                Some(DocumentMetadata::default()),
-            )
+#[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
+#[cfg_attr(not(target_arch = "wasm32"), async_trait)]
+impl DIDResolver for DIDExampleStatic {
+    async fn resolve(
+        &self,
+        did: &str,
+        _input_metadata: &ResolutionInputMetadata,
+    ) -> (
+        ResolutionMetadata,
+        Option<Document>,
+        Option<DocumentMetadata>,
+    ) {
+        match did {
+            "did:key:z6MkjF6Srb2uTSHVtjA53e59pUWJEY2QZzMkh9w198mhZmzB" => {
+                let doc = match Document::from_json(include_str!("../did-example-mine.json")) {
+                    Ok(doc) => doc,
+                    Err(e) => {
+                        return (
+                            ResolutionMetadata::from_error(&format!(
+                                "Unable to parse DID document: {:?}",
+                                e
+                            )),
+                            None,
+                            None,
+                        );
+                    }
+                };
+                (
+                    ResolutionMetadata::default(),
+                    Some(doc),
+                    Some(DocumentMetadata::default()),
+                )
+            }
+            _ => return (ResolutionMetadata::from_error(ERROR_NOT_FOUND), None, None),
         }
     }
 }
